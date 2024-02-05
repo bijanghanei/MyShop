01997c9096f4469eabdf7cc22872b67a9ebb4b89
bijanghanei <iambijanghanei@gmail.com>
2024-02-05 15:12:49 +0330

    Entity framework completed

---
+++ MyShop/MyShopDataAccess.SQL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopDataAccess.SQL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductCategory> categories { get; set; }
+    }
+}
---
+++ MyShop/MyShopDataAccess.SQL/Migrations/202402050858243_Intial.cs
@@ -0,0 +1,42 @@
+﻿namespace MyShopDataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Intial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Category = c.String(),
+                        dateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        Image = c.String(),
+                        dateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
