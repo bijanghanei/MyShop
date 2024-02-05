3d4e4fab64cc3a71da80354d8b4e1b7af9a44535
bijanghanei <iambijanghanei@gmail.com>
2024-02-05 23:46:57 +0330

    product listing completed

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,32 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productRepository;
+        IRepository<ProductCategory> productCategoryRepository;
+
+        public HomeController(IRepository<Product> productRepository, IRepository<ProductCategory> productCategoryRepository)
+        {
+            this.productRepository = productRepository;
+            this.productCategoryRepository = productCategoryRepository;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = productRepository.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = productRepository.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
