862ab639a1c9648ad93d36a619126f9a22b7d9c0
bijanghanei <iambijanghanei@gmail.com>
2024-02-06 08:46:09 +0330

    product filtering feature added

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,22 @@ namespace MyShop.WebUI.Controllers
             this.productCategoryRepository = productCategoryRepository;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string category=null)
         {
-            List<Product> products = productRepository.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategoryRepository.Collection().ToList();
+            if(category == null)
+            {
+                products = productRepository.Collection().ToList();
+            }
+            else
+            {
+                products = productRepository.Collection().Where(product => product.Category == category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.products = products;
+            model.categories = categories; 
+            return View(model);
         }
 
         public ActionResult Details(string Id)
